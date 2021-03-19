1bf418b273fc961131acdd1e98d05738dd542486
Max Zhang <maxzhang@max-zhang-macbook.local>
2021-03-18 22:39:54 -0700

    create-react-app + layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Waveform Visualizer</h1>
+      
+      <code class="bottom">written by Max Zhang, <a href="https://github.com/chetbae" rel="noreferrer" target="_blank">GitHub</a></code>
+    </div>
+  );
+}
+
+export default App;
